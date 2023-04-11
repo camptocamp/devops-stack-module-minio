37875c4b2d67abeec7ab9d07f48f82e49338d20c
Osman Mohammed <osman.mohammed@camptocamp.com>
2023-04-11 15:13:23 +0200

    feat: add initial terraform configuration and deploy cluster KinD

--- main.tf
+++ main.tf
@@ -1,94 +1,26 @@
-resource "null_resource" "dependencies" {
-  triggers = var.dependency_ids
+locals {
+  cluster_name = "tpi"
 }
 
-resource "argocd_project" "this" {
-  metadata {
-    name      = "<CHART_NAME>"
-    namespace = var.argocd_namespace
-    annotations = {
-      "devops-stack.io/argocd_namespace" = var.argocd_namespace
-    }
-  }
+module "kind" {
+  source = "git::https://github.com/camptocamp/devops-stack-module-cluster-kind.git?ref=v2.1.0"
 
-  spec {
-    description  = "<CHART_NAME> application project"
-    source_repos = ["https://github.com/camptocamp/devops-stack-module-<CHART_NAME>.git"]
-
-    destination {
-      name      = "in-cluster"
-      namespace = "kube-system"
-    }
-
-    orphaned_resources {
-      warn = true
-    }
-
-    cluster_resource_whitelist {
-      group = "*"
-      kind  = "*"
-    }
-  }
+  cluster_name       = local.cluster_name
+  kubernetes_version = "v1.24.7"
 }
 
-data "utils_deep_merge_yaml" "values" {
-  input = [for i in concat(local.helm_values, var.helm_values) : yamlencode(i)]
+provider "kubernetes" {
+  host               = module.kind.parsed_kubeconfig.host
+  client_certificate = module.kind.parsed_kubeconfig.client_certificate
+  client_key         = module.kind.parsed_kubeconfig.client_key
+  insecure           = true
 }
 
-resource "argocd_application" "this" {
-  metadata {
-    name      = "<CHART_NAME>"
-    namespace = var.argocd_namespace
+provider "helm" {
+  kubernetes {
+    host               = module.kind.parsed_kubeconfig.host
+    client_certificate = module.kind.parsed_kubeconfig.client_certificate
+    client_key         = module.kind.parsed_kubeconfig.client_key
+    insecure           = true
   }
-
-  timeouts {
-    create = "15m"
-    delete = "15m"
-  }
-
-  wait = var.app_autosync == { "allow_empty" = tobool(null), "prune" = tobool(null), "self_heal" = tobool(null) } ? false : true
-
-  spec {
-    project = argocd_project.this.metadata.0.name
-
-    source {
-      repo_url        = "https://github.com/camptocamp/devops-stack-module-<CHART_NAME>.git"
-      path            = "charts/<CHART_NAME>"
-      target_revision = var.target_revision
-      helm {
-        values = data.utils_deep_merge_yaml.values.output
-      }
-    }
-
-    destination {
-      name      = "in-cluster"
-      namespace = "kube-system"
-    }
-
-    sync_policy {
-      automated = var.app_autosync
-
-      retry {
-        backoff = {
-          duration     = ""
-          max_duration = ""
-        }
-        limit = "0"
-      }
-
-      sync_options = [
-        "CreateNamespace=true"
-      ]
-    }
-  }
-
-  depends_on = [
-    resource.null_resource.dependencies,
-  ]
-}
-
-resource "null_resource" "this" {
-  depends_on = [
-    resource.argocd_application.this,
-  ]
 }
---
+++ version.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    keycloak = {
+      source = "mrparkers/keycloak"
+      version = "~> 4"
+    }
+    argocd = {
+      source  = "oboukili/argocd"
+      version = "~> 4"
+    }
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+    }
+  }
+}
