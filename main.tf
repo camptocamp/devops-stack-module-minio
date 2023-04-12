ed4058828ef918be08e9b509514228981ed77211
Osman Mohammed <osman.mohammed@camptocamp.com>
2023-04-12 09:24:13 +0200

    docs: add comment to version.tf - main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,10 @@
+# In this file we call the modules of our use case of DevOps Stack.
+# locals are used to define comman variables for external use.
 locals {
   cluster_name = "tpi"
 }
 
+# Module KinD with cluster name and version
 module "kind" {
   source = "git::https://github.com/camptocamp/devops-stack-module-cluster-kind.git?ref=v2.1.0"
 
@@ -9,6 +12,7 @@ module "kind" {
   kubernetes_version = "v1.24.7"
 }
 
+# Usage of helm provider with cluster access credientials
 provider "helm" {
   kubernetes {
     host               = module.kind.parsed_kubeconfig.host
@@ -18,6 +22,7 @@ provider "helm" {
   }
 }
 
+# metallb module is used to have a service type loadbalancer for the cluster KinD
 module "metallb" {
   source = "git::https://github.com/camptocamp/devops-stack-module-metallb.git?ref=v1.0.1"
 
--- version.tf
+++ version.tf
@@ -1,5 +1,7 @@
+# Here we define all the required provides for the modules 
 terraform {
   required_providers {
+    # We use offical helm provider of hashicorp for modules like ArgoCD bootstrap and metallb with specific version contraint.
     helm = {
       source  = "hashicorp/helm"
       version = "~> 2"
