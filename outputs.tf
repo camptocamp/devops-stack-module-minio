24236675e37cc8ce310d401ab4e60422e0991ddf
Osman Mohammed <osman.mohammed@camptocamp.com>
2023-04-17 15:51:10 +0200

    feat: output minio endpoint

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,11 @@ output "id" {
   description = "ID to pass other modules in order to refer to this module as a dependency."
   value       = resource.null_resource.this.id
 }
+
+output "endpoint" {
+  description = "MinIO endpoint for where the buckets are available."
+  value       = "minio.${var.namespace}:9000"
+}
 output "minio_root_user_credentials" {
   value     = random_password.minio_root_secretkey
   sensitive = true
