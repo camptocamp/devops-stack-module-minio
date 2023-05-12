6b23a32f7dd88f884192d2c8b4ad098999808d67
Gonçalo Heleno <goncalo.heleno@camptocamp.com>
2023-05-12 09:56:04 +0200

    chore: general comment cleanup and improve README.adoc

--- locals.tf
+++ locals.tf
@@ -1,15 +1,12 @@
-# Helm chart value overrides. They should be passed as a list of HCL structures
-# Important we don't use the default helm values it's blank instead locals used to override it with custom values.
 locals {
   helm_values = [{
     minio = {
-      mode = "standalone" # set the deployment mode of MinIO to standalone
+      mode = "standalone" # Set the deployment mode of MinIO to standalone
       resources = {
         requests = {
-          memory = "128Mi" # specify the memory request for the container
+          memory = "128Mi"
         }
       }
-      # specify the ingress configuration for the MinIO console UI
       consoleIngress = {
         enabled = true
         annotations = {
@@ -20,12 +17,10 @@ locals {
           "ingress.kubernetes.io/ssl-redirect"               = "true"
           "kubernetes.io/ingress.allow-http"                 = "false"
         }
-        # specify the hostnames that the ingress should respond to
         hosts = [
           "minio.apps.${var.base_domain}",
           "minio.apps.${var.cluster_name}.${var.base_domain}",
         ]
-        # specify the TLS configuration for the ingress
         tls = [{
           secretName = "minio-tls"
           hosts = [
@@ -41,10 +36,9 @@ locals {
       }
       rootUser     = "root"
       rootPassword = random_password.minio_root_secretkey.result
-      # call created config_minio variable to use it the chart 
-      users    = var.config_minio.users
-      buckets  = var.config_minio.buckets
-      policies = var.config_minio.policies
+      users        = var.config_minio.users
+      buckets      = var.config_minio.buckets
+      policies     = var.config_minio.policies
     }
   }]
 }
