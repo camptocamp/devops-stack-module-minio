a8b1fa477acc5fcb63a956ee77c0ca9e10859acb
Gonçalo Heleno <goncalo.heleno@camptocamp.com>
2023-12-21 18:28:58 +0100

    fix: hardcode the release name to remove the destination cluster
    I found out that Argo CD passes the name of the application as a value to set the Helm chart. This means that all the templating that used `{ $.Release.Name }` would resolve to the name given to Argo CD application.
    
    In a multicluster deployment, using a single Argo CD, the names of the applications must be different. We solved that by appending the cluster name to the default application name when deploying on different clusters than `in-cluster`. However, this resulted in multiple problems for deployments that depended on the name of the application being static, so this solves that.

--- main.tf
+++ main.tf
@@ -67,7 +67,8 @@ resource "argocd_application" "this" {
       path            = "charts/minio"
       target_revision = var.target_revision
       helm {
-        values = data.utils_deep_merge_yaml.values.output
+        release_name = "minio"
+        values       = data.utils_deep_merge_yaml.values.output
       }
     }
 
